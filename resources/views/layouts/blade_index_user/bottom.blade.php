4fa3868d363d6ce1d2f5077d87b061f30192b674
HuyKaisoul <57024318+HuyKaisoul@users.noreply.github.com>
2020-05-17 07:43:13 +0700

    font-end_17/05
    xxx

--- resources/views/layouts/blade_index_user/bottom.blade.php
+++ resources/views/layouts/blade_index_user/bottom.blade.php
@@ -7,7 +7,7 @@
       <div class="container">
         <div class="row mb-5">
           <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3>Search Trending</h3>
+            <h3>Từ khóa phổ biến</h3>
             <ul class="list-unstyled">
               <li><a href="#">Web Design</a></li>
               <li><a href="#">Graphic Design</a></li>
@@ -18,24 +18,24 @@
             </ul>
           </div>
           <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3>Company</h3>
+            <h3>Trang web</h3>
             <ul class="list-unstyled">
-              <li><a href="#">About Us</a></li>
-              <li><a href="#">Career</a></li>
+              <li><a href="#">Giới thiệu</a></li>
               <li><a href="#">Blog</a></li>
-              <li><a href="#">Resources</a></li>
+              <!--<li><a href="#">Career</a></li>
+              <li><a href="#">Resources</a></li>-->
             </ul>
           </div>
           <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3>Support</h3>
+            <h3>Hỗ trợ</h3>
             <ul class="list-unstyled">
-              <li><a href="#">Support</a></li>
-              <li><a href="#">Privacy</a></li>
-              <li><a href="#">Terms of Service</a></li>
+              <li><a href="#">Hỗ trợ</a></li>
+              <li><a href="#">Riêng tư</a></li>
+              <li><a href="#">Dịch vụ</a></li>
             </ul>
           </div>
           <div class="col-6 col-md-3 mb-4 mb-md-0">
-            <h3>Contact Us</h3>
+            <h3>Liên hệ</h3>
             <div class="footer-social">
               <a href="#"><span class="icon-facebook"></span></a>
               <a href="#"><span class="icon-twitter"></span></a>
@@ -49,7 +49,7 @@
           <div class="col-12">
             <p class="copyright"><small>
               <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-            Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="icon-heart text-danger" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank" >Colorlib</a>
+            Bản quyền &copy;<script>document.write(new Date().getFullYear());</script> được thực hiện với <i class="icon-heart text-danger" aria-hidden="true"></i><a href="https://colorlib.com" target="_blank" >Team TMT</a>
             <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></small></p>
           </div>
         </div>
